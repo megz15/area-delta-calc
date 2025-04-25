314f84887a9d2ea058cf5b5d6e0b27f8c72aed49
megz15 <meghraj.g16@gmail.com>
2025-04-25 23:05:48 +0530

    fix: scale

--- deltacalc.py
+++ deltacalc.py
@@ -1,15 +1,15 @@
 import ee           # api documentation referred from
 ee.Authenticate()   # https://developers.google.com/earth-engine/apidocs
-ee.Initialize(project = "ee-ww-1")                          # this is the project I"ve created
-                                                            # throuh my google developer console
+ee.Initialize(project = "ee-ww-1")  # this is the project I"ve created
+                                    # throuh my google developer console
 
 bits_audi_coords = [78.5706124275664, 17.545331001776212]   # roughly the auditorium coords
 bits_audi = ee.Geometry.Point(bits_audi_coords)             # from google maps
 bits_audi_buffer = bits_audi.buffer(5000)                   # 5 km radius
 
-init_date      = ee.Date("2025-02-20")                      # february 20 2025
-one_year_ago   = init_date.advance(-1, "year")              # february 20 2024
-five_years_ago = init_date.advance(-5, "year")              # february 20 2020
+init_date      = ee.Date("2025-04-01")          # april 1 2025
+one_year_ago   = init_date.advance(-1, "year")  # april 1 2024
+five_years_ago = init_date.advance(-5, "year")  # april 1 2020
 
 param_names = [
     "trees",
@@ -32,11 +32,14 @@ five_year_dw = dw_col(five_years_ago)
 def calc_area_change(old_col, new_col):
     old_area = old_col.multiply(ee.Image.pixelArea()).reduceRegion( # multiply by pixel area
         reducer=ee.Reducer.sum(), # sum to get total area           # to get area in m^2
-        geometry=bits_audi_buffer
+        geometry=bits_audi_buffer,
+        scale = 10, # scale is 10m for dynamic world data
+        maxPixels=1e13
     )
     new_area = new_col.multiply(ee.Image.pixelArea()).reduceRegion( # reducer is efficient for
         reducer=ee.Reducer.sum(),                                   # aggregating pixel values
-        geometry=bits_audi_buffer                                   # over large area as this
+        geometry=bits_audi_buffer,                                  # over large area as this
+        scale = 10, maxPixels=1e13
     )
     return {band: (new_area.get(band).getInfo() - old_area.get(band).getInfo()) / 10000 # hectares
             for band in param_names}
