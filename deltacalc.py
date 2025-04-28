cb5fae6cb3d600bd79bfd44a6aab39fce18ba143
megz15 <meghraj.g16@gmail.com>
2025-04-28 17:06:55 +0530

    feat: flask

--- deltacalc.py
+++ deltacalc.py
@@ -1,14 +1,13 @@
+from flask import Flask, request, jsonify, render_template
 import ee           # api documentation referred from
 ee.Authenticate()   # https://developers.google.com/earth-engine/apidocs
 ee.Initialize(project = "ee-ww-1")
 
+app = Flask(__name__)
+
 bits_audi_coords = [78.32674059291753, 17.4534271]          # UoH lon, lat
 bits_audi = ee.Geometry.Point(bits_audi_coords)             # from google maps
 bits_audi_buffer = bits_audi.buffer(5000)                   # 5 km radius
-
-# init_date      = ee.Date("2025-04-10")          # april 10 2025
-# one_year_ago   = init_date.advance(-1, "year")  # april 10 2024
-# five_years_ago = init_date.advance(-5, "year")  # april 10 2020
 
 before_end = ee.Date("2025-01-28")  # january 28 2025
 after_end = ee.Date("2025-04-28")   # april 28 2025
@@ -20,50 +19,49 @@ param_names = [
     "shrub_and_scrub"
 ]   # parameters to be used
 
-def dw_col(end_date):  # function to get specific dynamic world data
+def dw_col(end_date, region):  # function to get specific dynamic world data
     img = (ee.ImageCollection("GOOGLE/DYNAMICWORLD/V1")
-        .filterBounds(bits_audi_buffer)
+        .filterBounds(region)
         .filterDate(end_date.advance(-3, "month"), end_date)
         .select(param_names)
         .mean())
     mask = img.reduce(ee.Reducer.max()).gt(0.2)  # remove noise
     return img.updateMask(mask)
 
-def calc_area_change(old_col, new_col):
+def calc_area_change(old_col, new_col, region):
     old_area = old_col.multiply(ee.Image.pixelArea()).reduceRegion( # multiply by pixel area
         reducer=ee.Reducer.sum(), # sum to get total area           # to get area in m^2
-        geometry=bits_audi_buffer,
+        geometry=region,
         scale = 10, # scale is 10m for dynamic world data
         maxPixels=1e13
     )
     new_area = new_col.multiply(ee.Image.pixelArea()).reduceRegion( # reducer is efficient for
         reducer=ee.Reducer.sum(),                                   # aggregating pixel values
-        geometry=bits_audi_buffer,                                  # over large area as this
+        geometry=region,                                  # over large area as this
         scale = 10, maxPixels=1e13
     )
     return {band: (new_area.get(band).getInfo() - old_area.get(band).getInfo()) / 10000 # hectares
             for band in param_names}
 
-# current_dw   = dw_col(init_date)
-# one_year_dw  = dw_col(one_year_ago)
-# five_year_dw = dw_col(five_years_ago)
+@app.route('/')
+def index():
+    return render_template('index.html', lon = bits_audi_coords[0], lat = bits_audi_coords[1], 
+                           before_end = "01-28-2025", after_end = "04-28-2025")
 
-before_dw = dw_col(before_end)
-after_dw = dw_col(after_end)
+@app.route('/calculate', methods=['GET'])
+def calculate():
+    before_end = request.args.get('before', default="2025-01-28", type=str)
+    after_end = request.args.get('after', default="2025-04-28", type=str)
+    lat = request.args.get('lat', default=17.4534271, type=float)
+    lon = request.args.get('lon', default=78.32674059291753, type=float)
 
-# five_year_change = calc_area_change(five_year_dw, current_dw)
-# one_year_change = calc_area_change(one_year_dw, current_dw)
+    region = ee.Geometry.Point([lon, lat])
+    region_b = region.buffer(5000)   
 
-before_after_change = calc_area_change(before_dw, after_dw)
+    before_dw = dw_col(ee.Date(before_end), region_b)
+    after_dw = dw_col(ee.Date(after_end), region_b)
+    before_after_change = calc_area_change(before_dw, after_dw, region_b)
 
-# print("Change in area (hectares) over the last 5 years:")
-# for band, change in five_year_change.items():
-#     print(f"{band}: {change:.2f}")
+    return jsonify(before_after_change)
 
-# print("\nChange in area (hectares) over the last year:")
-# for band, change in one_year_change.items():
-#     print(f"{band}: {change:.2f}")
-
-print("Change in area (hectares):")
-for band, change in before_after_change.items():
-    print(f"{band}: {change:.2f}")
+app.run(debug=True)
